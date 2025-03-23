9877f5606ebc30df5c90301f28a604f7c2399d1e
Fofanova Anastasiya <portable.tem@gmail.com>
2025-03-24 01:36:16 +0300

    fix book_store_test

--- src/test/java/tests/BooksStoreTestBase.java
+++ src/test/java/tests/BooksStoreTestBase.java
@@ -1,19 +1,17 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.*;
 
 public class BooksStoreTestBase {
     @BeforeAll
@@ -25,6 +23,7 @@ public class BooksStoreTestBase {
         RestAssured.baseURI = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") + "/wd/hub";
+        Configuration.timeout = 10000;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map .<String, Object>of(
@@ -36,23 +35,12 @@ public class BooksStoreTestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
-    @BeforeEach
-    void addListener() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
-
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
-    }
-
-    @AfterAll
-    static void clearAll() {
-        clearBrowserCookies();
-        clearBrowserLocalStorage();
-        closeWebDriver();
+        Selenide.closeWebDriver();
     }
 }
